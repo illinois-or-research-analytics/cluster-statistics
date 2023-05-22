4d6400bd9b63c7d0d0e46b7c7fe11405cd42e28d
vikramr2 <vikramr2@illinois.edu>
2023-05-22 06:06:23 +0000

    batch processing statistics

--- batch.sh
+++ batch.sh
@@ -1,30 +1,18 @@
-symlink=$(find $1 -type l -lname '*' | grep -q . && echo true || echo false)
+#!/bin/bash
+global_graph=$1
 
-# files=$(ls -p "$1" | grep -v /)
+# Read the specified column into a shell array
+clusterings=($(awk -F' ' '{print $1}' config.tsv))
+resolutions=($(awk -F' ' '{print $2}' config.tsv))
+clusterers=($(awk -F' ' '{print $3}' config.tsv))
 
-files=()
+# Print the elements of the array
+arr_length=(${#clusterers[@]})
+for ((index=0; index<$arr_length; index++))
+do
+    clustering="${clusterings[index]}"
+    resolution="${resolutions[index]}"
+    clusterer="${clusterers[index]}"
 
-# Store files in the directory in the array
-for file in "$1"/*; do
-    if [ -f "$file" ]; then
-        files+=("$file")
-    fi
-done
-
-if $symlink; then
-    for ((i = 0; i < ${#files[@]}; i++)); do
-        file="${files[$i]}"
-        file_path="$1/$file"
-        if [ -L "$file_path" ]; then
-            true
-            # echo "$file_path is a symbolic link. Keeping it in the array."
-        else
-            # echo "Removing $file from the array."
-            unset 'files[i]'
-        fi
-    done
-fi
-
-for file in "${files[@]}"; do
-    echo $file
+    python3 stats.py -i $global_graph -e $clustering -c $clusterer -g $resolution
 done
